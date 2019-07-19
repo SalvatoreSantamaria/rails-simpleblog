e2f9bc6fad4c83739df0c60a7e55f0a97555f565
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-07-18 22:01:35 -0700

    End of thursdays work

--- simpleblog/config/routes.rb
+++ simpleblog/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #Root Route
   root 'posts#index'
 
   #Example of a regular route with get request. it goes to the page controller
