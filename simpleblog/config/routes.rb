ac731f35820376e1d3a0bc12b55bdde4e75aeb95
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-07-18 21:48:50 -0700

    added about page

--- simpleblog/config/routes.rb
+++ simpleblog/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
+
+  #Example of a regular route with get request. it goes to the page controller
+  get 'about' => 'pages#about'
 end
