5088433cdfbd4032225a7c1bd92ae6f50a8537d9
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-07-20 09:25:25 -0700

    Added Bootstrap and initial homepage styling

--- simpleblog/config/routes.rb
+++ simpleblog/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #Root Route
-  root 'posts#index'
+  root 'posts#index', as: 'home'
 
   #Example of a regular route with get request. it goes to the page controller
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about', as: 'about'
 
   #Example resource route with options
   resources :posts
