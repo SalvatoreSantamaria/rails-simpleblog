59d925da60a54801c79e816bf5dd9e4b87f81630
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-07-20 13:11:11 -0700

    added deug params

--- simpleblog/app/controllers/comments_controller.rb
+++ simpleblog/app/controllers/comments_controller.rb
@@ -4,6 +4,7 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
+    
   end
 
   def destroy
